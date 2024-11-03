42d6826fe7f0a4ccd92683530f8691d418508fe1
Anastasiia <kaspertasya@gmail.com>
2024-11-03 15:54:20 +0300

    add HomeWork 2.1, 2.2

---
+++ HW2.1/Home2.1.cpp
@@ -0,0 +1,115 @@
+﻿/* Тема: Классы и объекты.Задача 1, Калькулятор
+В этом задании вам нужно будет создать класс, выполняющий простые арифметические операции для двух чисел, и продемонстрировать его работу
+Создайте класс Calculator. Класс должен содержать два поля num1 и num2 для хранения дробных чисел. 
+Также класс должен содержать следующие методы:
+
+double add() - метод должен возвращать результат сложения num1 и num2
+double multiply() - метод должен возвращать результат перемножения num1 и num2
+double subtract_1_2() - метод должен возвращать результат вычитания num2 из num1
+double subtract_2_1() - метод должен возвращать результат вычитания num1 из num2
+double divide_1_2() - метод должен возвращать результат деления num1 на num2
+double divide_2_1() - метод должен возвращать результат деления num2 на num1
+bool set_num1(double num1) - метод должен установить значение поля num1 равное значению аргумента num1 в случае, если значение аргумента не равно 0. Возвращает true, если установка произошла успешно, в обратном случае false
+bool set_num2(double num2) - метод должен установить значение поля num2 равное значению аргумента num2 в случае, если значение аргумента не равно 0. Возвращает true, если установка произошла успешно, в обратном случае false
+Напишите код, демонстрирующий работу класса Calculator. Попросите пользователя ввести два числа. 
+После этого выведите пользователю результаты всех операций или попросите его ввести значение ещё раз, если предыдущий ввод был некорректен
+
+Программа спрашивает пользователя бесконечно, пока он сам не закроет консоль*/
+
+#include <iostream>
+#include <string>
+
+class Calculator
+{
+private:
+    double num1;
+    double num2;
+
+public:
+    double get_num1() { return num1; }
+    double get_num2() { return num2; }
+    
+    Calculator() { num1 = 0; num2 = 0; }
+
+//Методы
+    double add()//сложение
+    {
+        return num1 + num2;
+    };
+    double multiply()//умножение
+    {
+        return num1 * num2;
+    };
+    double subtract_1_2()//вычитание из первого
+    {
+        return num1 - num2;
+    };
+    double subtract_2_1()//вычитание из второго
+    {
+        return num2 - num1;
+    };
+    double divide_1_2()//деление первого на второе
+    {
+        return num1 / num2;
+    };
+    double divide_2_1()//деление второго на первое
+    {
+        return num2 / num1;
+    };
+    bool set_num1(double val1)//
+    {
+        if (val1 == 0)
+        {
+            return false;
+        }
+        else
+        {
+            num1 = val1;
+            return true;
+        }
+    };
+    bool set_num2(double val2)//
+    {
+        if (val2 == 0)
+        {
+            return false;
+        }
+        else
+        {
+            num2 = val2;
+            return true;
+        }
+    };
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    Calculator Seasson;
+    double user_num1=0, user_num2=0;
+
+    while (true)
+    {
+        std::cout << "Введите первое число (num1): ";
+        std::cin >> user_num1;
+        std::cout << "Введите второе число (num2): ";
+        std::cin >> user_num2;
+
+        if (Seasson.set_num1(user_num1) == false or Seasson.set_num2(user_num2) == false)
+        {
+            std::cout << "Неверный ввод чисел" << std::endl;
+        }
+        else
+        {
+            std::cout << "num1 + num2 = " << Seasson.add() << std::endl;
+            std::cout << "num1 * num2 = " << Seasson.multiply() << std::endl;
+            std::cout << "num1 - num2 = " << Seasson.subtract_1_2() << std::endl;
+            std::cout << "num2 - num1 = " << Seasson.subtract_2_1() << std::endl;
+            std::cout << "num1 / num2 = " << Seasson.divide_1_2() << std::endl;
+            std::cout << "num2 / num1 = " << Seasson.divide_2_1() << std::endl;
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
---
+++ HW2.2/Home2.2.cpp
@@ -0,0 +1,95 @@
+﻿/* Тема: Классы и объекты.Задача 2, Счетчик
+В этом задании вам нужно будет создать класс-счётчик
+Создайте класс Counter. Класс должен выполнять функцию счётчика, для этого он должен предоставлять:
+    возможность увеличить своё значение на 1
+    возможность уменьшить своё значение на 1
+    возможность посмотреть своё текущее значение
+    возможность создания экземпляра класса с начальным значением по умолчанию (1)
+    возможность создания экземпляра класса с инициализирующим начальным значением
+Обратите внимание на то, что у внешнего кода не должно быть возможности изменить внутреннее значение счётчика напрямую
+Напишите код, демонстрирующий работу класса Counter. Спросите у пользователя, хочет ли он указать начальное значение для счётчика.
+Если хочет, дайте ему возможность это сделать. Затем предоставьте пользователю возможность вводить команды:
+    +: увеличьте счётчик на 1
+    -: уменьшите счётчик на 1
+    =: выведите текущее значение счётчика
+    x: завершите работу программы */
+
+#include <iostream>
+#include <string>
+
+class Counter
+{
+private:
+    int num;
+
+public:
+    int get_num() { return num; }
+    void set_num(int val) { num = val; }
+    
+    Counter() { num = 1; };
+    Counter(int val=1) { num = val; };
+
+//Методы
+    void increase()
+    {
+        num = num + 1;
+    };
+
+    void decrease()
+    {
+        num = num - 1;
+    };
+    void result()
+    {
+        std::cout << num << std::endl;
+    };
+
+    bool closeWork()
+    {
+        return false;
+    };
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+ 
+    std::string choice_start, command;
+    int val=1;
+    bool status_work = true;
+
+    std::cout << "Счетчик активирован." << std::endl << "Доступные команды: '+'  увеличение на 1, '-' уменьшение на 1, '=' печать текущего значения, '*' завершение работы" << std::endl;
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите 'yes' или 'not': ";
+    std::cin >> choice_start;
+
+    if (choice_start == "yes")
+    {
+        std::cout << "Введите начальное значение : ";
+        std::cin >> val;  
+    }
+    else if (choice_start == "not")
+    {
+        std::cout << "Работа счетчика начнется с 1 " << std::endl;
+
+    }
+    else
+    {
+        std::cout << "Команда не распознана. Работа завершена " << std::endl;
+        status_work = false;
+    }    
+   
+    Counter userCounter(val);
+
+    while (status_work == true)
+    {
+        std::cout << "Введите команду('+', '-', '=' или '*') : ";
+        std::cin >> command;
+        if (command == "+") { userCounter.increase();}
+        else if (command == "-") { userCounter.decrease(); }
+        else if (command == "=") { userCounter.result(); }
+        else if (command == "*") { status_work = userCounter.closeWork(); }
+        else { std::cout << "Команда не распознана" << std::endl; }
+    }
+
+    return 0;
+}
